3a789ce8f28305b9debbc55be386148bbdda0edc
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-10 03:15:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,1 @@
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
-    using_push = (colors_in_the_rainbow, next_color)
+updated_array = using_push(colors_in_the_rainbow, next_color)
